ea454088198f13d64b93617f4593aacda3ac7f63
2020-009 <109118819+2020-009@users.noreply.github.com>
2022-07-19 09:04:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,19 @@ input.onButtonPressed(Button.A, function () {
     计数 += 1
     basic.showNumber(计数)
 })
-// 1.每按一下Bbutton计数+2
-// 2.显示计数
+// 1.计数会倒数直到计数=0
+// 2.显示图标
+// 3.显示times out
 input.onButtonPressed(Button.AB, function () {
-    计数 += 2
-    basic.showNumber(计数)
+    倒数 = 计数
+    for (let index = 0; index < 倒数; index++) {
+        计数 += -1
+        basic.showNumber(计数)
+    }
+    basic.showIcon(IconNames.Sad)
+    basic.pause(100)
+    basic.showString("times out")
+    basic.pause(100)
 })
 // 1.每按一下Bbutton计数+2
 // 2.显示计数
@@ -20,5 +28,6 @@ input.onButtonPressed(Button.B, function () {
 // 2.清空屏幕
 let 计数 = 0
 let 倒数 = 0
+倒数 = 0
 计数 = 0
 basic.clearScreen()
