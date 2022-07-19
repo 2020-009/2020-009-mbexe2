ff27ffbf493de36cd06c86ab87b1aa38349601ba
2020-009 <109118819+2020-009@users.noreply.github.com>
2022-07-19 08:52:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,24 @@
+// 1.每按一下Abutton计数+1
+// 2.显示计数
+input.onButtonPressed(Button.A, function () {
+    计数 += 1
+    basic.showNumber(计数)
+})
+// 1.每按一下Bbutton计数+2
+// 2.显示计数
+input.onButtonPressed(Button.AB, function () {
+    计数 += 2
+    basic.showNumber(计数)
+})
+// 1.每按一下Bbutton计数+2
+// 2.显示计数
+input.onButtonPressed(Button.B, function () {
+    计数 += 2
+    basic.showNumber(计数)
+})
 // 1.设置变量-计数、倒数
 // 2.清空屏幕
-basic.forever(function () {
-	
-})
+let 计数 = 0
+let 倒数 = 0
+计数 = 0
+basic.clearScreen()
